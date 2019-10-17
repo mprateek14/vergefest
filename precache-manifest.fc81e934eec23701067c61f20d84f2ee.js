efba89a141733f16d5c703174f2a34b1797377d6
mprateek14 <prateekmadan14@gmail.com>
2019-10-17 14:15:54 +0530

    Updates

---
+++ precache-manifest.fc81e934eec23701067c61f20d84f2ee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1758a58344f53274a1b83054838234a",
+    "url": "/vergefest/index.html"
+  },
+  {
+    "revision": "3eefe1fbb1831011c9ef",
+    "url": "/vergefest/static/css/2.a9909ecd.chunk.css"
+  },
+  {
+    "revision": "6bd376c16de29f2674bd",
+    "url": "/vergefest/static/css/main.2a334b5d.chunk.css"
+  },
+  {
+    "revision": "3eefe1fbb1831011c9ef",
+    "url": "/vergefest/static/js/2.8671d26b.chunk.js"
+  },
+  {
+    "revision": "6bd376c16de29f2674bd",
+    "url": "/vergefest/static/js/main.867fb0f6.chunk.js"
+  },
+  {
+    "revision": "98dfc43881fe02042de6",
+    "url": "/vergefest/static/js/runtime-main.34a79cd7.js"
+  },
+  {
+    "revision": "222b8e6849b3a3a66c0bb7002b0e4603",
+    "url": "/vergefest/static/media/72397.222b8e68.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vergefest/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vergefest/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vergefest/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vergefest/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vergefest/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0a81424db9ea1190b1c36952f842a0f3",
+    "url": "/vergefest/static/media/fpp.0a81424d.png"
+  }
+]);
