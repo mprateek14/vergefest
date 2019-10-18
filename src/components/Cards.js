ec91befe2d9fddd9f82ec7231721a96a6fed9010
mprateek14 <prateekmadan14@gmail.com>
2019-10-19 01:55:54 +0530

    transitions added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,9 +10,9 @@ export default class Cards extends Component {
         <div>
             <div className="container-fluid d-flex justify-content-center">
                 <div className="row">
-                    <div className="col-md-4" data-aos="slide-right" data-aos-duration="1300" data-aos-once="true"><Card name={eventList[0].name} description={eventList[0].description} imageUrl={eventList[0].imageUrl} /></div>
+                    <div className="col-md-4" data-aos="slide-right" data-aos-duration="1100" data-aos-once="true"><Card name={eventList[0].name} description={eventList[0].description} imageUrl={eventList[0].imageUrl} /></div>
                     <div className="col-md-4"><Card name={eventList[1].name} description={eventList[1].description} imageUrl={eventList[1].imageUrl}/></div>
-                    <div className="col-md-4 " data-aos="slide-left" data-aos-duration="1300" data-aos-once="true"><Card name={eventList[2].name} description={eventList[2].description} imageUrl={eventList[2].imageUrl}/></div>
+                    <div className="col-md-4 " data-aos="slide-left" data-aos-duration="1100" data-aos-once="true"><Card name={eventList[2].name} description={eventList[2].description} imageUrl={eventList[2].imageUrl}/></div>
                     
                 </div>
                
@@ -20,9 +20,9 @@ export default class Cards extends Component {
 
             <div className="container-fluid d-flex justify-content-center" style={{paddingBottom:"4rem"}}>
                 <div className="row">
-                    <div className="col-md-4" data-aos="slide-right" data-aos-duration="1300" data-aos-once="true"><Card name={eventList[3].name} description={eventList[3].description} imageUrl={eventList[3].imageUrl}/></div>
+                    <div className="col-md-4" data-aos="slide-right" data-aos-duration="1100" data-aos-once="true"><Card name={eventList[3].name} description={eventList[3].description} imageUrl={eventList[3].imageUrl}/></div>
                     <div className="col-md-4"><Card name={eventList[4].name} description={eventList[4].description} imageUrl={eventList[4].imageUrl}/></div>
-                    <div className="col-md-4" data-aos="slide-left" data-aos-duration="1300" data-aos-once="true"><Card name={eventList[5].name} description={eventList[5].description} imageUrl={eventList[5].imageUrl}/></div>
+                    <div className="col-md-4" data-aos="slide-left" data-aos-duration="1100" data-aos-once="true"><Card name={eventList[5].name} description={eventList[5].description} imageUrl={eventList[5].imageUrl}/></div>
                     
                     
                 </div>
